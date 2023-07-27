bb25efa6c0843add9892c7d26bf39cccdd54af49
krsaurabh1322 <55047250+krsaurabh1322@users.noreply.github.com>
2023-07-28 02:20:02 +0530

    Update LogInPython.py

--- LogInPython.py
+++ LogInPython.py
@@ -24,6 +24,19 @@ def log_execution_time(func):
         return result
 
     return wrapper
+
+def log_info(message):
+    logging.info(message)
+
+def log_warning(message):
+    logging.warning(message)
+
+def log_error(message):
+    logging.error(message)
+
+def log_critical(message):
+    logging.critical(message)
+
 #--------------------------------
 
 
